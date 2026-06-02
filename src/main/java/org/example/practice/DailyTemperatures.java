11c32668d4867974b750104aa36da5bff758eef8
liuxingyu.164 <liuxingyu.164@jd.com>
2026-06-02 16:40:31 +0800

    feat: 每日温度

---
+++ src/main/java/org/example/practice/DailyTemperatures.java
@@ -0,0 +1,21 @@
+package org.example.practice;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
