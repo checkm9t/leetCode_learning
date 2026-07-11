40fa5776610d2680458ddc49786db9fa240a690f
liuxingyu.164 <liuxingyu.164@jd.com>
2026-07-11 15:53:43 +0800

    feat:轮转数组

---
+++ src/main/java/org/example/practice/Rotate.java
@@ -0,0 +1,23 @@
+package org.example.practice;
+
+public class Rotate {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        // 这个很重要，防止数组越界
+        k = k%n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+
+    public void reverse(int[] nums,int left,int right){
+        while(left<right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
