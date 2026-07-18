9684a17b30b919345ee243d215322d4536129308
liuxingyu.164 <liuxingyu.164@jd.com>
2026-07-18 18:53:24 +0800

    feat:矩阵置0

--- src/main/java/org/example/practice/SetZeroes.java
+++ src/main/java/org/example/practice/SetZeroes.java
@@ -1,4 +1,40 @@
 package org.example.practice;
 
 public class SetZeroes {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++) {
+            // 先判断第一列是不是0
+            if (matrix[i][0] == 0) {
+                col0 = 0;
+            }
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // 因为第0行必须留到最后处理，所以是倒序
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 最后处理第0列
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        return;
+    }
 }
