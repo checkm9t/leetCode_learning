27f3782e5f71ade32f80c929f402724854d8af54
liuxingyu.164 <liuxingyu.164@jd.com>
2026-07-31 14:10:31 +0800

    feat:组合总和

---
+++ src/main/java/org/example/practice/MaxPathSum.java
@@ -0,0 +1,28 @@
+package org.example.practice;
+
+import org.example.common.TreeNode;
+
+public class MaxPathSum {
+    public int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxPath(root);
+        return res;
+    }
+
+    /**
+     从下任意节点到node的最大值
+     */
+    public int maxPath(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftMax = Math.max(maxPath(node.left), 0);
+        int rightMax = Math.max(maxPath(node.right), 0);
+
+        res = Math.max(res, leftMax + rightMax + node.val);
+
+        return Math.max(leftMax, rightMax) + node.val;
+    }
+}
