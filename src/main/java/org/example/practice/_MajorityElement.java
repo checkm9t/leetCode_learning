0d510f6006d23da153c06b1712131c75107e538b
liuxingyu.164 <liuxingyu.164@jd.com>
2026-08-05 16:32:46 +0800

    feat:杨辉三角

---
+++ src/main/java/org/example/practice/_MajorityElement.java
@@ -0,0 +1,22 @@
+package org.example.practice;
+
+public class _MajorityElement {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int cnt = 0;
+
+        for (int num : nums) {
+            if (cnt == 0) {
+                candidate = num;
+            }
+
+            if (candidate == num) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+
+        return cnt == 0 ? -1 : candidate;
+    }
+}
