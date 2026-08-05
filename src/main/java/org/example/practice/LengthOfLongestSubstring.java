a5f5282d1c18830a2be57b57f2849004157ee46e
liuxingyu.164 <liuxingyu.164@jd.com>
2026-08-05 09:40:01 +0800

    feat:

--- src/main/java/org/example/practice/LengthOfLongestSubstring.java
+++ src/main/java/org/example/practice/LengthOfLongestSubstring.java
@@ -9,6 +9,7 @@ public class LengthOfLongestSubstring {
         int res = 0;
 
         while (right < s.length()) {
+            // 这里需要循环，先找左边，再找右边
             while (counts[s.charAt(right)] > 0 && left <= right) {
                 counts[s.charAt(left)]--;
                 left++;
---
+++ src/main/java/org/example/practice/isPalindrome.java
@@ -0,0 +1,32 @@
+package org.example.practice;
+
+import org.example.common.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class isPalindrome {
+
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> arr = new ArrayList<>();
+
+        ListNode cur = head;
+        while (cur != null) {
+            arr.add(cur.val);
+            cur = cur.next;
+        }
+
+        int front = 0;
+        int tail = arr.size() - 1;
+
+        while (front < tail) {
+            if (arr.get(front) != arr.get(tail)) {
+                return false;
+            }
+            front++;
+            tail--;
+        }
+
+        return true;
+    }
+}
